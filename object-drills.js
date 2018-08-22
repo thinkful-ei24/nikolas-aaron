009274fb702941f3636e88816383c7436c9fc087
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-22 09:00:50 -0700

    second object drill

--- object-drills.js
+++ object-drills.js
@@ -1,10 +1,27 @@
+'use strict';
+
 function createMyObject() {
-    return {
-        foo: "bar",
-        answerToUniverse : 42,
-        "olly olly": "oxen free",
-        sayHello: function() {
-            return "Hello"
-        }
+  return {
+    foo: "bar",
+    answerToUniverse: 42,
+    "olly olly": "oxen free",
+    sayHello: function () {
+      return "Hello"
     }
+  };
 }
+
+
+function updateOject(obj) {
+  obj.foo = 'foo';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  obj.bar = 'bar';
+  return obj;
+}
+
+let myObj = {
+
+};
+
+console.log(updateOject(myObj));
