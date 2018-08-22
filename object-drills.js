530dfffa48ae5d4c086bd447521ee6625ce963b6
Nikolas Melgarejo <nikoclops@gmail.com>
2018-08-22 12:03:59 -0400

    completed 3rd obj drill

--- object-drills.js
+++ object-drills.js
@@ -20,8 +20,21 @@ function updateOject(obj) {
   return obj;
 }
 
-let myObj = {
+// let myObj = {
 
-};
+// };
 
-console.log(updateOject(myObj));
+// console.log(updateOject(myObj));
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  };
+  return person;
+}
