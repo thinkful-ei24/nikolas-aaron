e9379db556c1ee3abded95f4f3cee5185dc9f218
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-22 14:26:32 -0700

    done for now

--- object-drills.js
+++ object-drills.js
@@ -295,3 +295,38 @@ function filterForHobbits () {
  }
 
  console.log(filterForAttacksGreaterThanFive());
+
+
+
+
+// Not finished with this yet
+
+
+//  const HEROES = [
+//   { id: 1, name: 'Captain America', squad: 'Avengers' },
+//   { id: 2, name: 'Iron Man', squad: 'Avengers' },
+//   { id: 3, name: 'Spiderman', squad: 'Avengers' },
+//   { id: 4, name: 'Superman', squad: 'Justice League' },
+//   { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+//   { id: 6, name: 'Aquaman', squad: 'Justice League' },
+//   { id: 7, name: 'Hulk', squad: 'Avengers' },
+// ];
+
+// function findOne(arr, query) {
+//   arr.find(obj => {
+//     if (obj.id === query) {
+//       return obj;
+//     }
+//     else if (obj.name === query) {
+//       return obj;
+//     }
+//     else if (obj.squad === query) {
+//       return obj;
+//     }
+//     else {
+//       return null;
+//     }
+//   })
+// }
+
+// console.log(findOne(HEROES, {id: 1}));
