37295e507684530e832280f61678cb0065a01ad0
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-22 09:06:34 -0700

    fourth drill done

--- object-drills.js
+++ object-drills.js
@@ -38,3 +38,18 @@ function personMaker() {
   };
   return person;
 }
+
+
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
