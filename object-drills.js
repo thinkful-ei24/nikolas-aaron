beafd7e626a739a806f6be98ac7814a97d7a7195
Nikolas Melgarejo <nikoclops@gmail.com>
2018-08-22 11:54:46 -0400

    did the very first function called createMyObject

---
+++ object-drills.js
@@ -0,0 +1,10 @@
+function createMyObject() {
+    return {
+        foo: "bar",
+        answerToUniverse : 42,
+        "olly olly": "oxen free",
+        sayHello: function() {
+            return "Hello"
+        }
+    }
+}
